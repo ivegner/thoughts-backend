4344d8a4a9862ea742c2901ae7e37a7e122545e4
Vanya Vegner <Vanya Vegner>
2015-11-26 16:08:37 -0500

    Image and Text input to GCS

--- thoughts/thoughts.py
+++ thoughts/thoughts.py
@@ -1,31 +1,36 @@
-import random
-from google.appengine.api import users
+import random	#for the index
 from google.appengine.ext import ndb
 import endpoints
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
+import cloudstorage as gcs	#in the lib folder
 
 package = 'Thoughts'	#package name for organization
 
-class ThoughtText(messages.Message):
-	text = messages.StringField(1)		#only has one parameter which is a string with ID 1
-	testNum = messages.FloatField(2)	#debug returns the number used as random point
+bucketName = "thoughtsubmissions"
+
+############### API Part Starts ##############################
+
+class IOThought(messages.Message):
+	text = messages.StringField(1, default = None)		#string param with ID 1
+	testNum = messages.FloatField(2) #debug returns the number used as random point
+	img = messages.BytesField(3, default = None)
 	
-@endpoints.api(name='thoughts', version='v3')	#API declaration decorator
+@endpoints.api(name='thoughts', version='v4')	#API declaration decorator
 class ThoughtsApi(remote.Service):
 
-	THOUGHT_TEXT_RESOURCE = endpoints.ResourceContainer(ThoughtText)	#basically a thing that handles passed in URL parameters I think
+	RETURN_THOUGHT_RESOURCE = endpoints.ResourceContainer(IOThought)	#basically a thing that handles passed in URL parameters I think
 	
-	@endpoints.method(THOUGHT_TEXT_RESOURCE,ThoughtText,path="thoughtput",http_method='POST',name='put')
-	#THOUGHT_TEXT_RESOURCE is what the method expects in the request, ThoughtText is the actual request, path is the url path to the method, 
+	@endpoints.method(RETURN_THOUGHT_RESOURCE,IOThought,path="putThought",http_method='POST',name='put')
+	#RETURN_THOUGHT_RESOURCE is what the method expects in the request, IOThought is the actual request, path is the url path to the method, 
 	#http_method is the method (GET or POST) and the name is the name of the method in the API, which seems kinda redundant but is necessary
 	def thought_put(self, request):
-		putThought(request.text) #calls function that actually puts the text passed into the request in DB
-		return ThoughtText(text=request.text) #debug?
+		debug = putThought(request.text, request.img) #calls function that actually puts the text passed into the request in DB
+		return debug #debug?
 		
-	@endpoints.method(message_types.VoidMessage, ThoughtText, path='getThought', http_method='GET',name='getThought')
-	#as above, only ThoughtText here is the response
+	@endpoints.method(message_types.VoidMessage, IOThought, path='getThought', http_method='GET',name='getThought')
+	#as above, only IOThought here is the response
 	def thought_get(self, request):
 		thought=getThought()
 		return thought
@@ -33,14 +38,26 @@ class ThoughtsApi(remote.Service):
 ############### Database Interaction Part Starts ###############
 
 class Thought(ndb.Model):	#database entity class with a bunch of properties
-	text = ndb.StringProperty(indexed=False)
+	gcsObjectName = ndb.StringProperty(indexed=False)
 	date = ndb.DateTimeProperty(auto_now_add=True)
 	index = ndb.FloatProperty()	#random index for random outputting
 
-def putThought(textIn):
-	thought = Thought(text=textIn, index=random.random())
-	thought.put()
-	return thought
+def putThought(textIn, imgIn):
+	randIndex = random.random()
+	filename = "/" + bucketName + "/" + str(randIndex)
+	if textIn != None:
+		with gcs.open(filename, mode="w", content_type="text/plain") as f:	#makes file in bucketName with name randIndex
+			f.write(str(textIn))
+			dbEntry = Thought(gcsObjectName=filename, index = randIndex)
+			dbEntry.put()
+	elif imgIn != None:
+		with gcs.open(filename, "w", content_type="image/png") as f:	#makes file in bucketName with name randIndex
+			f.write(imgIn)
+			dbEntry = Thought(gcsObjectName=filename, index = randIndex)
+			dbEntry.put()
+	else:
+		return IOThought(text="No Submission Made")
+	return IOThought(text="Submission Made")
 
 def getThought():
 	randNum=random.random()
@@ -48,10 +65,12 @@ def getThought():
 	test = thought.get()	#this runs the query and gets the first result. use fetch($num) to get $num results
 	if test is None:		#if randNum is higher than any indices
 		thought = Thought.query().filter(Thought.index <= randNum).order(Thought.index) #reverse 
-		test = thought.get()
-		return ThoughtText(text=test.text, testNum=randNum)
+		test = getFromDB(thought.get())
+		return IOThought(text=test, testNum=randNum)
 	else:
 		print "\n\nThe query was: " + str(thought) #displays after server is shut down
-		return ThoughtText(text=test.text, testNum=randNum)	#response
+		return IOThought(text=test.text, testNum=randNum)	#response
+def getFromDB(thoughtQueryResult):
+	return thoughtQueryResult.text
 	
 APPLICATION = endpoints.api_server([ThoughtsApi]) #passed into app.yaml to actually start the API 
